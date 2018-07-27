0c8a5215cf4992a416f5f43170e51570b9819f38
Chris Dorward <listingslab@gmail.com>
2018-07-28 08:11:20 +1000

    Hosted on firebase, leveraging all kinds of Google goodness (#2)

---
+++ public/config.js
@@ -0,0 +1,16 @@
+config = {
+    "appName": "listingslab",
+    "version": "0.1.2",
+    "react": {
+        "localhost": "http://localhost:3000",
+        "prod": "https://react-tech-test.firebaseapp.com"
+    },
+    "firebase": {
+        "apiKey": "AIzaSyCoOZHVS7Z95vdRz6IBE8nItiirBFrADC8",
+        "authDomain": "react-tech-test.firebaseapp.com",
+        "databaseURL": "https://react-tech-test.firebaseio.com",
+        "projectId": "react-tech-test",
+        "storageBucket": "react-tech-test.appspot.com",
+        "messagingSenderId": "977847849436"
+    }
+};
